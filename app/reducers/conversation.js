8ee3eabd242b97aca82eefaf4d39bd032b4d4eff
DanCLee <dan.clee1218@gmail.com>
2016-08-04 12:33:50 -0700

    changes in conversation.js

--- app/reducers/conversation.js
+++ app/reducers/conversation.js
@@ -39,7 +39,7 @@ function getStep(key, defaultStep){
 			.then(() => simpleStore.get( key ))
 			.then( msgVar => {
 				console.log("NO store: APPARENTLY THE STEP IS " + msgVar.step);
-				return msgVar.step;
+				promiseResults(msgVar.step);
 			})
 			.catch(error => {
 				console.log(error.message)
@@ -53,7 +53,7 @@ function getStep(key, defaultStep){
 			.then( msgVar => {
 				var theStep = msgVar.step;
 				console.log("Yes store: APPARENTLY THE STEP IS " + theStep);
-				return theStep;
+				promiseResults(theStep);
 			})
 			.catch(error => {
 				console.log(error.message)
@@ -65,6 +65,12 @@ function getStep(key, defaultStep){
 	//console.log("Inside getStep: We are now playing this game " + key + " " + defaultStep);
 
 
+}
+
+
+function promiseResults(val) {
+
+	console.log("Here is the value: woot: " + val);
 
 }
 
@@ -108,65 +114,14 @@ export default function conversationreducer(state = initialState, action = {}) {
 			};
 		case types.SETSTEP:
 
-			var defaultStep = action.defaultStep;
-			var key = action.key;
+			getStep(action.key, action.defaultStep);
 
-			checkStore(action.key, returnStep)
 
-			function checkStore(k, callback){
 
-				var msgVar = this[k];
-
-				simpleStore.get(k)
-				.then( (msgVar) => {
-					console.log(msgVar == null);
-					callback(msgVar == null);
-				});
-
-				//return true; 
-			}
-
-			function returnStep( truth ){
-				if( truth ) {
-					// if checkStore returns false, no store. Then create a store
-					var msgVar = this[key];
-
-					simpleStore.save( key, {
-						step: defaultStep
-					})
-					.then(() => simpleStore.get( key ))
-					.then( msgVar => {
-						console.log("NO store: APPARENTLY THE STEP IS " + msgVar.step);
-						return {
-							...state,
-							currStep: msgVar.step
-						};
-						//return msgVar.step;
-					})
-					.catch(error => {
-						console.log(error.message)
-					});
-				} else {
-					console.log("THERE APPEARS TO BE A STORE");
-
-					var msgVar = this[key]
-
-					simpleStore.get( key )
-					.then( msgVar => {
-						var theStep = msgVar.step;
-						console.log("Yes store: APPARENTLY THE STEP IS " + theStep);
-						return {
-							...state,
-							currStep: theStep
-						};
-
-						//return theStep;
-					})
-					.catch(error => {
-						console.log(error.message)
-					});
-				}
-			}
+			// return {
+			// 	...state,
+			// 	currStep: 
+			// };
 							
 
 		default:
